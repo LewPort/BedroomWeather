3f2d8b38b7b2926c7487bff36bc15688d2277a7f
Lewis Porter <porter.lewis@gmail.com>
2025-03-08 02:47:15 -0500

    butts

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     t, h, time, human_time = dht.get_live_data(4)
-    return render_template("index.html", human_time, t, h)
+    return render_template("index.html", human_time=human_time, t=t, h=h)
