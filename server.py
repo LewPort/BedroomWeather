437f9151dc67fe473d9444a746154fca9b34bbbe
LewPort <porter.lewis@gmail.com>
2025-03-10 01:00:33 -0400

    add outdoor info to website html/flask server

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask import render_template
 import dht_operations as dht
-from sql_operations import return_list_from_db as outdoor
+import sql_operations as sql
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     indoor_temp, indoor_humid, time, indoor_human_time = dht.get_live_data(4)
-    outdoor_time, outdoor_human_time, outdoor_temp, outdoor_humid, outdoor_pressure, outdoor_wind_speed, outdoor_wind_dir = outdoor('outdoorweather', 1)
+    outdoor_time, outdoor_human_time, outdoor_temp, outdoor_humid, outdoor_pressure, outdoor_wind_speed, outdoor_wind_dir = sql.return_list_from_db('outdoorweather', 1)
     return render_template("index.html",
                            human_time=indoor_human_time,
                            indoor_temp=indoor_temp,
