9197be6def7bb9262f0721a8d4322afe6c8b7048
LewPort <porter.lewis@gmail.com>
2025-03-10 00:52:45 -0400

    fix website background image file path

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import render_template
 import dht_operations as dht
+from sql_operations import return_list_from_db as outdoor
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    t, h, time, human_time = dht.get_live_data(4)
-    return render_template("index.html", human_time=human_time, temp=t, humid=h)
+    indoor_temp, indoor_humid, time, indoor_human_time = dht.get_live_data(4)
+    outdoor_time, outdoor_human_time, outdoor_temp, outdoor_humid, outdoor_pressure, outdoor_wind_speed, outdoor_wind_dir = outdoor('outdoorweather', 1)
+    return render_template("index.html",
+                           human_time=indoor_human_time,
+                           indoor_temp=indoor_temp,
+                           indoor_humid=indoor_humid,
+                           outdoor_temp=outdoor_temp,
+                           outdoor_humid=outdoor_humid,
+                           outdoor_pressure=outdoor_pressure,
+                           outdoor_wind_speed=outdoor_wind_speed,
+                           outdoor_wind_dir=outdoor_wind_dir)
