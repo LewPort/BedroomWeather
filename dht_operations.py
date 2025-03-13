5b75feb95cb85f20fb87b34eb9ec39ca71b8cf04
LewPort <porter.lewis@gmail.com>
2025-03-12 22:23:52 -0400

    add json file maker

--- dht_operations.py
+++ dht_operations.py
@@ -10,7 +10,7 @@ def get_live_data(pin):
     human_time = time.ctime(time_logged) #Create a human-readable version of time_logged
     h = round(h, 2) #Round them to 2 decimal places
     t = round(t, 2)
-    return t, h, time_logged, human_time #return all that info as tuple
+    return (t, h, time_logged, human_time) #return all that info as tuple
 
 def write_to_json(t, h, time_logged, human_time):
     with open('./json/dht.json', 'w') as f:
